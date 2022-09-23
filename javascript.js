e75d6d9c4e597b87b0722d272495288be631452d
krystianzun <krystian.zun@gmail.com>
2022-09-23 22:27:31 +0100

    Fix Cannot read property addEventListener of null.

--- javascript.js
+++ javascript.js
@@ -1,17 +1,14 @@
-const paperButton = document.getElementById('paper');
-const paraText = document.getElementById('para');
+const btn = document.querySelector('button');
+const txt = document.querySelector('p');
 
+btn.addEventListener('click', updateBtn);
 
-// Test if buttons work. Text shows up on any button click. 
-paperButton.addEventListener('click', setText);
-
-
-
-const btn = document.getElementById('theButton');
-const myText = document.getElementById('theDiv');
-
-btn.addEventListener('click', function(){
-  const myInsertText = 'Hello World !';
-  myText.innerHTML = myInsertText;
-
-});
+function updateBtn() {
+  if (btn.textContent === 'Start machine') {
+    btn.textContent = 'Stop machine';
+    txt.textContent = 'The machine has started!';
+  } else {
+    btn.textContent = 'Start machine';
+    txt.textContent = 'The machine is stopped.';
+  }
+}
